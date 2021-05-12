d6929d2a3c80c0e76a8c2644053c2ede759e80b3
Andras Racz <andras.racz@ni.com>
2021-05-12 13:50:38 +0200

    Everything

--- diamonds.py
+++ diamonds.py
@@ -23,33 +23,140 @@ from sklearn.pipeline import make_pipeline;
 from sklearn.preprocessing import StandardScaler;
 
 #Getting the diamonds
-diamonds = pd.read_csv('https://raw.githubusercontent.com/raczandras/ML/main/diamonds.csv', usecols=range(1,11));
+diamonds = pd.read_csv('https://raw.githubusercontent.com/raczandras/ML/main/diamonds.csv');
 
-#Replacing attributes to simple numbers
-cols = ['price'];
-cheap = diamonds['price'] <= 2000;
-medium = diamonds['price'] <= 9000;
-expensive = diamonds ['price'] > 9000;
-diamonds['price'] = np.select([cheap, medium, expensive], [1,2,3], default=-1);
-diamonds['cut'] = diamonds.cut.mask(diamonds.cut=="Fair", 1);
-diamonds['cut'] = diamonds.cut.mask(diamonds.cut=="Good", 2);
-diamonds['cut'] = diamonds.cut.mask(diamonds.cut=="Ideal", 3);
-diamonds['cut'] = diamonds.cut.mask(diamonds.cut=="Premium", 4);
-diamonds['cut'] = diamonds.cut.mask(diamonds.cut=="Very Good", 5);
+atts = diamonds.drop('pricerange',axis=1);
+atts2 = atts.to_numpy();
+att_names = atts.columns;
+target = diamonds['pricerange'];
+target2 = target.copy();
+target = target.replace(to_replace = 1, value = "cheap");
+target = target.replace(to_replace = 2, value = "average");
+target = target.replace(to_replace = 3, value = "expensive");
+target_names = ["cheap", "average", "expensive"];
+diamonds2 = diamonds;
 
-diamonds['color'] = diamonds.color.mask(diamonds.color=="D", 1);
-diamonds['color'] = diamonds.color.mask(diamonds.color=="E", 2);
-diamonds['color'] = diamonds.color.mask(diamonds.color=="F", 3);
-diamonds['color'] = diamonds.color.mask(diamonds.color=="G", 4);
-diamonds['color'] = diamonds.color.mask(diamonds.color=="H", 5);
-diamonds['color'] = diamonds.color.mask(diamonds.color=="I", 6);
-diamonds['color'] = diamonds.color.mask(diamonds.color=="J", 7);
+columns_titles= ["carat", "price", "x_length", "y_length", "z_length"];
 
-diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="I1", 1);
-diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="IF", 2);
-diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="SI1", 3);
-diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="SI2", 4);
-diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="VS1", 5);
-diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="VS2", 6);
-diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="VVS1", 7);
-diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="VVS2", 8);
+#PCA
+pipe = make_pipeline(StandardScaler(), PCA());
+diamonds_pc_scaled=pipe.fit_transform(atts)
+var_ratio = pipe.named_steps['pca'].explained_variance_ratio_;
+cum_var_ratio = np.cumsum(var_ratio);
+plt.figure();
+plt.title('Variance of attribute');
+df = pd.DataFrame({'var':pipe.named_steps['pca'].explained_variance_ratio_,
+             'PC':att_names})
+sns.barplot(x='PC',y="var", 
+           data=df, color="c");
+plt.show();
+
+plt.figure();
+plt.title('CUM Variance');
+df = pd.DataFrame({'var':cum_var_ratio,
+             'PC':att_names})
+sns.barplot(x='PC',y="var", 
+           data=df, color="c");
+plt.show();
+
+
+#Scatter for attributes
+colors = ['blue','red','green'];
+plt.figure();
+p = atts.shape[1]; # number of attributes
+feature_selection = SelectKBest(k=2);
+feature_selection.fit(atts,target);
+scores = feature_selection.scores_; #attribute weight
+features1 = feature_selection.transform(atts);
+mask = feature_selection.get_support();
+feature_indices = [];
+for i in range(p):
+    if mask[i] == True : feature_indices.append(i);
+x_axis, y_axis = feature_indices;
+print('Importance weight of input attributes')
+for i in range(p):
+    print(features1[i],': ',scores[i]);
+plt.title('Scatterplot for 2 kbest attribs');
+plt.xlabel(att_names[x_axis]);
+plt.ylabel(att_names[y_axis]);
+plt.scatter(atts2[:,0],atts2[:,1],s=50,c=target2, cmap=col.ListedColormap(colors));
+plt.show();
+
+#Divide data to test and train data
+X_train, X_test, y_train, y_test = train_test_split(atts, target, test_size=0.2, 
+                                shuffle = True, random_state=2021);
+
+# Fitting logistic regression
+logreg_classifier = LogisticRegression(solver='liblinear')
+logreg_classifier.fit(X_train,y_train)
+score_train_logreg = logreg_classifier.score(X_train,y_train)
+ypred_logreg = logreg_classifier.predict(X_train)
+cm_logreg_train = confusion_matrix(y_train, ypred_logreg); # train confusion matrix
+score_test_logreg = logreg_classifier.score(X_test,y_test)
+ypred_logreg = logreg_classifier.predict(X_test)
+cm_logreg_test = confusion_matrix(y_test, ypred_logreg); # test confusion matrix
+yprobab_logreg = logreg_classifier.predict_proba(X_test)
+scores = cross_val_score(logreg_classifier, atts, target, cv=5)
+print("Logistic regression scores:")
+print(scores)
+#Results of logistic regression
+plot_confusion_matrix(logreg_classifier, X_train, y_train, display_labels = target_names);
+plt.title('Confusion matrix for TRAIN data (logreg)');
+plot_confusion_matrix(logreg_classifier, X_test, y_test, display_labels = target_names);
+plt.title('Confusion matrix for TEST data (logreg)');
+
+
+#Fitting decision tree
+depth = 3;
+class_tree = DecisionTreeClassifier(criterion = "gini", max_depth=depth, min_samples_leaf=15)
+class_tree.fit(X_train, y_train)
+score_train = class_tree.score(X_train, y_train)
+score_test = class_tree.score(X_test, y_test)
+y_pred_gini = class_tree.predict(X_test)
+yprobab_tree = class_tree.predict_proba(X_test)
+fig = plt.figure(8,figsize = (12,6),dpi=100);
+plot_tree(class_tree, feature_names = att_names, 
+               class_names = target_names,
+               filled = True, fontsize = 6)
+
+plot_confusion_matrix(class_tree, X_train, y_train, display_labels = target_names);
+plt.title('Confusion matrix for TRAIN data (decision tree)');
+plot_confusion_matrix(class_tree, X_test, y_test, display_labels = target_names);
+plt.title('Confusion matrix for TEST data (decision tree)');
+
+#Contingency
+# Finding optimal cluster number
+Max_K = 12;  # maximum cluster number
+SSE = np.zeros((Max_K-2));
+DB = np.zeros((Max_K-2));
+for i in range(Max_K-2):
+    n_c = i+2;
+    kmeans = KMeans(n_clusters=n_c, random_state=2020);
+    kmeans.fit(atts);
+    labels = kmeans.labels_;
+    SSE[i] = kmeans.inertia_;
+    DB[i] = davies_bouldin_score(atts,labels);
+    
+# Visualization of SSE values    
+plt.figure();
+plt.title('Sum of squares of error curve');
+plt.xlabel('Number of clusters');
+plt.ylabel('SSE');
+plt.plot(np.arange(2,Max_K),SSE, color='red')
+plt.show();
+
+# Visualization of DB scores
+plt.figure();
+plt.title('Davies-Bouldin score curve');
+plt.xlabel('Number of clusters');
+plt.ylabel('DB index');
+plt.plot(np.arange(2,Max_K),DB, color='blue')
+plt.show();
+
+#KMEANS with optimal clusters
+kmeans = KMeans(n_clusters=3, random_state=2020);
+kmeans.fit(X_test);
+data_labels = kmeans.labels_;
+cm = contingency_matrix(y_test, kmeans.labels_);
+
+
