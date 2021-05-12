f8562c1c518ccd68d0f59cb7e2735a2c0f2003e2
Andras Racz <andras.racz@ni.com>
2021-05-12 11:44:21 +0200

    Replaced non number attributes to numbers

--- diamonds.py
+++ diamonds.py
@@ -23,4 +23,33 @@ from sklearn.pipeline import make_pipeline;
 from sklearn.preprocessing import StandardScaler;
 
 #Getting the diamonds
-diamonds = pd.read_csv('https://raw.githubusercontent.com/raczandras/ML/main/diamonds.csv');
+diamonds = pd.read_csv('https://raw.githubusercontent.com/raczandras/ML/main/diamonds.csv', usecols=range(1,11));
+
+#Replacing attributes to simple numbers
+cols = ['price'];
+cheap = diamonds['price'] <= 2000;
+medium = diamonds['price'] <= 9000;
+expensive = diamonds ['price'] > 9000;
+diamonds['price'] = np.select([cheap, medium, expensive], [1,2,3], default=-1);
+diamonds['cut'] = diamonds.cut.mask(diamonds.cut=="Fair", 1);
+diamonds['cut'] = diamonds.cut.mask(diamonds.cut=="Good", 2);
+diamonds['cut'] = diamonds.cut.mask(diamonds.cut=="Ideal", 3);
+diamonds['cut'] = diamonds.cut.mask(diamonds.cut=="Premium", 4);
+diamonds['cut'] = diamonds.cut.mask(diamonds.cut=="Very Good", 5);
+
+diamonds['color'] = diamonds.color.mask(diamonds.color=="D", 1);
+diamonds['color'] = diamonds.color.mask(diamonds.color=="E", 2);
+diamonds['color'] = diamonds.color.mask(diamonds.color=="F", 3);
+diamonds['color'] = diamonds.color.mask(diamonds.color=="G", 4);
+diamonds['color'] = diamonds.color.mask(diamonds.color=="H", 5);
+diamonds['color'] = diamonds.color.mask(diamonds.color=="I", 6);
+diamonds['color'] = diamonds.color.mask(diamonds.color=="J", 7);
+
+diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="I1", 1);
+diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="IF", 2);
+diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="SI1", 3);
+diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="SI2", 4);
+diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="VS1", 5);
+diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="VS2", 6);
+diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="VVS1", 7);
+diamonds['clarity'] = diamonds.clarity.mask(diamonds.clarity=="VVS2", 8);
